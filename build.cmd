9a3fe3534d08502df5e8bd5c67cbff35db2435bf
AndriusK <andrius.k@sanitex.eu>
2018-11-12 09:04:15 +0200

    Com recompile

--- build.cmd
+++ build.cmd
@@ -1,9 +1,5 @@
 
-set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
-@if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
-@if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\15.0\Bin\MSBuild.exe"
-@if not exist %MSBuild% @set MSBuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
-@if not exist %MSBuild% @set MSBuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
+set MSBuild="c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
 %MSBuild% VfpProj.sln /v:m
 
